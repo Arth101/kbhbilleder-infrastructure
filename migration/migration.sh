968aaced6f7c54eb7da9e321de152e793e9b9ee1
Asbjørn Dyhrberg Thegler <asbjoern@thegler.dk>
2021-10-05 10:03:59 +0200

    Updates migration script

--- migration/migration.sh
+++ migration/migration.sh
@@ -1,13 +1,13 @@
-kc config use-context gke_kbh-billeder_europe-west1-b_kbh-billeder-staging-cluster
+kubectl config use-context gke_kbh-billeder_europe-west1-b_kbh-billeder-staging-cluster
 
-kc exec -it -n production mongo-9ctjr -- mongodump --out=/tmp/dump
+kubectl exec -it -n production mongo-9ctjr -- mongodump --out=/tmp/dump
 
-kc cp -n production mongo-9ctjr:/tmp/dump ./dump
+kubectl cp -n production mongo-9ctjr:/tmp/dump ./dump
 
-kc config use-context gke_kbh-billeder_europe-west1-b_kbh-billeder-cluster-1
+kubectl config use-context gke_kbh-billeder_europe-west1-b_kbh-billeder-cluster-1
 
-kc cp -n production ./dump mongo-9fn74:/tmp/dump
+kubectl cp -n production ./dump mongo-9fn74:/tmp/dump
 
-kc exec -it -n production mongo-9fn74 -- mongorestore --drop /tmp/dump
+kubectl exec -it -n production mongo-9fn74 -- mongorestore --drop /tmp/dump
 
-kc apply -f ../kbh-billeder-cluster-1/manifest/production/kbhbilleder.dk.yaml
+kubectl apply -f ../kbh-billeder-cluster-1/manifest/production/kbhbilleder.dk.yaml
